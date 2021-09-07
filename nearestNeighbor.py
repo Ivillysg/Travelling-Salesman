abcc332d1c0b9559a2c81570b08adf6675ecf283
ivillysg <ivillysgomes@gmail.com>
2021-09-06 21:02:04 -0300

    🩹 fix sum last vertice selected

--- nearestNeighbor.py
+++ nearestNeighbor.py
@@ -1,3 +1,4 @@
+from inputs.readFile import readFile
 import sys
 from timeit import default_timer as timer
 
@@ -13,11 +14,11 @@ def nearestNeighbor(matrix, source):
     lenPath = 0
     arrayDistances = []
 
-    while lenPath < len(matrix[0]) - 1:
+    while lenPath < len(matrix[0])-1:
         for i in range(len(matrix[0])):
             if currentVertex != i and i not in visitedVertex:
-                if distanceNext > matrix[currentVertex][i]:
-                    distanceNext = matrix[currentVertex][i]
+                if distanceNext > float(matrix[currentVertex][i]):
+                    distanceNext = float(matrix[currentVertex][i])
                     nextVertex = i
 
         # 3. faça o vértice atual ser V.
@@ -29,6 +30,7 @@ def nearestNeighbor(matrix, source):
 
         # 5. se todos os vértices no domínio estiverem visitados, encerre o algoritmo.
         if lenPath == len(matrix[0]):
+
             break
         # 6. Se não vá para o passo 2.
         else:
@@ -36,19 +38,28 @@ def nearestNeighbor(matrix, source):
             arrayDistances.append(distanceNext)
             distanceNext = sys.maxsize
 
+    distanceLastToSource = float(matrix[visitedVertex[0]][nextVertex])
+                                  
+    arrayDistances.append(distanceLastToSource)
+    visitedVertex.append(nextVertex)                             
+    visitedVertex.append(visitedVertex[0])
+
     print('Menor custo =>', sum(arrayDistances), '\n')
-    print('Pontos Visitados =>', visitedVertex)
+    print('Melhor caminho =>', visitedVertex)
 
 
-matrix = [[0.0,  39.3,  37.2,  65.9,  56.3, 33.0, 51.0],
-          [39.0,  0.0,  38.9,  49.2,  79.4, 53.7, 68.2],
-          [37.2,  38.9,  0.0,  30.9,  43.0, 21.2, 30.4],
-          [65.9,  49.2,  30.9,  0.0,  64.1, 50.0, 50.6],
-          [56.5,  79.4,  43.0, 64.1, 0.0, 26.1, 13.6],
-          [33.0, 53.7, 21.2, 50.0, 26.1, 0.0, 18.0],
-          [51.0, 68.2, 30.4, 50.6, 13.6, 18.0, 0.0]]
+# matrix = [[0.0,  39.3,  37.2,  65.9,  56.3, 33.0, 51.0],
+#           [39.0,  0.0,  38.9,  49.2,  79.4, 53.7, 68.2],
+#           [37.2,  38.9,  0.0,  30.9,  43.0, 21.2, 30.4],
+#           [65.9,  49.2,  30.9,  0.0,  64.1, 50.0, 50.6],
+#           [56.5,  79.4,  43.0, 64.1, 0.0, 26.1, 13.6],
+#           [33.0, 53.7, 21.2, 50.0, 26.1, 0.0, 18.0],
+#           [51.0, 68.2, 30.4, 50.6, 13.6, 18.0, 0.0]]
+
+matrix = readFile('inputs/five_dist.txt')
+
 start = timer()
-nearestNeighbor(matrix, 3)
+nearestNeighbor(matrix, 0)
 end = timer()
 
 print(f'{end - start}s')
